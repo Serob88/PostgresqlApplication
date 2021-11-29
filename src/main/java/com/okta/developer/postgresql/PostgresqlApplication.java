3839ef82b93e8d0c37da72525e62ae82218e0e76
Serob <kirakosyan(serob.kirakosyan.88@gmail.com)>
2021-11-29 12:13:33 +0400

    Initial commit.

--- src/main/java/com/okta/developer/postgresql/PostgresqlApplication.java
+++ src/main/java/com/okta/developer/postgresql/PostgresqlApplication.java
@@ -2,9 +2,6 @@ package com.okta.developer.postgresql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
@@ -15,15 +12,4 @@ public class PostgresqlApplication {
         SpringApplication.run(PostgresqlApplication.class, args);
     }
 
-    @Configuration
-    static class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http
-                    .authorizeRequests().anyRequest().authenticated()
-                    .and()
-                    .oauth2ResourceServer().jwt();
-        }
-    }
 }
--- src/test/java/com/okta/developer/postgresql/PostgresqlApplicationTests.java
+++ src/test/java/com/okta/developer/postgresql/PostgresqlApplicationTests.java
@@ -9,8 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class PostgresqlApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+
 
 }
